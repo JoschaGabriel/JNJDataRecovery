cfd392e496550eea08e7f1dda8a95f84204af86c
joscha-gabriel-git <joschagabrielacm@gmail.com>
2023-10-29 09:49:26 +0700

    push pertama

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ""),
+    files: ["./src/*.{html,js}"],
+  },
+  theme: {
+    animatedSettings: {
+      animatedSpeed: 1000,
+      heartBeatSpeed: 500,
+      hingeSpeed: 2000,
+      bounceInSpeed: 750,
+      bounceOutSpeed: 750,
+      animationDelaySpeed: 500,
+      classes: ["bounce", "heartBeat"],
+    },
+    extend: {},
+    prefix: "tw-",
+  },
+  plugins: [require("taos/plugin", "tailwindcss-animatecss")],
+  safelist: ["!duration-[0ms]", "!delay-[0ms]", 'html.js :where([class*="taos:"]:not(.taos-init))'],
+};
